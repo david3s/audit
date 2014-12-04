c23a7029a5537651530eb9278f4ddbcff17a7b5a
Jerald Sheets <questy@gmail.com>
2014-12-03 23:43:01 -0800

    changed audit

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 # Main installation manifest for auditd
 class audit::install {
-  package {'auditd':
-    ensure => 'installed',
+  package {'audit':
+    ensure => "${profiles::audit_dev::installed}",
   }
 }
